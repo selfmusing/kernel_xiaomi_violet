fa246b72b7db242a3c186c9366b4237708ed078c
Tyler Nijmeh <tylernij@gmail.com>
2019-08-03 20:54:51 -0700

    debug_locks: Optionally remove debug
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- include/linux/debug_locks.h
+++ include/linux/debug_locks.h
@@ -12,7 +12,6 @@ struct task_struct;
 extern int debug_locks;
 extern int debug_locks_silent;
 
-
 static inline int __debug_locks_off(void)
 {
 	return xchg(&debug_locks, 0);
@@ -30,6 +29,14 @@ static inline int __debug_locks_off(void)
 #ifdef CONFIG_DEBUG_KERNEL
 extern int debug_locks_off(void);
 #else
+static const int debug_locks;
+static const int debug_locks_silent;
+
+static inline int __debug_locks_off(void)
+{
+	return 0;
+}
+
 static inline int debug_locks_off(void)
 {
 	return 0;
